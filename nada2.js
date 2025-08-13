efad5275bb25291acf7f38690fe5286b330f5640
Leandro <leandroaerodapt@gmail.com>
2025-08-13 18:04:12 -0300

    Adicionar nada2.js

---
+++ nada2.js
@@ -0,0 +1,13 @@
+ const numbers1 = [45, 4, 9, 16, 25];
+ const numbers2 = numbers1.map(value => {
+    return value * 2
+});
+console.log(numbers2)
+
+
+
+const numbers3 = [45, 4, 9, 16, 25];
+const numbers4 = numbers1.map(value => value * 2
+);
+console.log(numbers4)
+
